7326f7acfef7b70d75a9e275c7529c8c2fa86d70
luandro <luandro@gmail.com>
2019-04-21 14:06:19 -0300

    fixed protocol

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = ({ containers }) => (
   <Layout>
     <h1>Servidor da Comunidade</h1>
     {console.log(containers)}
-    {containers.map(c => <div key={c.Id}>
+    {containers && containers.map(c => <div key={c.Id}>
       <h3>{c.Image} - {c.State} {c.Status}</h3>
     </div>)}
     <style jsx>{`
@@ -19,8 +19,8 @@ const Home = ({ containers }) => (
 
 Home.getInitialProps = async ctx => {
   const apiUrl = process.browser
-    ? `${protocol}://${window.location.host}:6661/containers/list`
-    : `${protocol}://${ctx.req.headers.host}:6661/containers/list`
+    ? `http://${window.location.host}:6661/containers/list`
+    : `http://${ctx.req.headers.host}:6661/containers/list`
 
   try {
     const response = await fetch(apiUrl)
